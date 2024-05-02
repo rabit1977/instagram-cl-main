f0eb23b17292a140763f45d5c7cb222f3b1b0954
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2024-05-02 19:48:36 +0200

    more updates, working on loginForm, RegisterForm, forgotPassword, and more, from now...

--- pixelgram-yt-main/next-auth.d.ts
+++ pixelgram-yt-main/next-auth.d.ts
@@ -1,5 +1,7 @@
 import type { Session, User } from "next-auth";
 import type { JWT } from "@auth/core/jwt";
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
 
 declare module "next-auth/jwt" {
   interface JWT {
@@ -17,5 +19,17 @@ declare module "next-auth" {
 
   interface User {
     username?: string | null;
+  }
+}
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
   }
 }
